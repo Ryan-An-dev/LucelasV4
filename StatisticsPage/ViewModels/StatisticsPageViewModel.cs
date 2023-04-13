56c0b1cc2a910b1225efb4f244f1fe77bb177849
sjan <sjan@mpole.co.kr>
2023-04-13 17:02:39 +0900

    차트 결정

--- StatisticsPage/ViewModels/StatisticsPageViewModel.cs
+++ StatisticsPage/ViewModels/StatisticsPageViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using DataAccess;
 using DataAccess.NetWork;
-using LiveChartsCore;
 using Newtonsoft.Json.Linq;
 using Prism.Commands;
 using Prism.Ioc;
@@ -13,15 +12,14 @@ using System.Linq;
 using System.ComponentModel;
 using CommonModel;
 using CommonModel.Model;
-using SkiaSharp;
 using System.Text;
 using System.Drawing.Text;
 using System.Drawing;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
+using System.Collections.ObjectModel;
 using LiveCharts.Wpf;
 using LiveCharts;
-using System.Collections.ObjectModel;
 
 namespace StatisticsPage.ViewModels
 {
@@ -55,18 +53,16 @@ namespace StatisticsPage.ViewModels
 
     public class StatisticsPageViewModel : PrismCommonModelBase, INavigationAware, INetReceiver
     {
-        public ReactiveProperty<ObservableCollection<string>> Label { get; set; }
-        public ReactiveProperty<Axis> YAxes { get; set; }
+        public ObservableCollection<string> Label { get; set; }
+        
 
         public ReactiveProperty<StatisticsUnit> SelectedUnit { get; set; }
         public IEnumerable<StatisticsUnit> StatisticsUnitList
         {
             get { return Enum.GetValues(typeof(StatisticsUnit)).Cast<StatisticsUnit>(); }
         }
-        public ReactiveProperty<LineSeries> PreviousMonth { get; set; }
-        public ReactiveProperty<LineSeries> ThisMonth { get; set; }
 
-        public ISeries[] SeriesCollection { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
         private IContainerProvider ContainerProvider { get; }
 
         public void ChangedChartUnit(StatisticsUnit args) {
@@ -74,39 +70,48 @@ namespace StatisticsPage.ViewModels
         }
         public StatisticsPageViewModel(IRegionManager regionManager, IContainerProvider containerProvider) : base(regionManager)
         {
-            this.Label = new ReactiveProperty<ObservableCollection<string>>().AddTo(this.disposable);
+            this.Label = new ObservableCollection<string>();
+            this.SeriesCollection = new SeriesCollection(); 
             this.SelectedUnit = new ReactiveProperty<StatisticsUnit>(0).AddTo(this.disposable);
             this.ContainerProvider = containerProvider;
             this.SelectedUnit.Subscribe(x => { ChangedChartUnit(x); });
-
-            IList<int?> values = new int?[100];
-            IList<int?> values2 = new int?[100];
+            ChartValues<double> values = new ChartValues<double>();
+            ChartValues<double> values2 = new ChartValues<double>();
             var r = new Random();
             var t = 0;
-
+           
             for (var i = 0; i < 100; i++)
             {
                 t += r.Next(0, 100);
-                values[i] = t;
+                values.Add(t);
             }
             for (var i = 0; i < 100; i++)
             {
                 if (i >= 51) {
-                    values2[i] = null;
+                    
                     continue;
                 }
                 t += r.Next(0, 100);
-                values2[i] = t;
+                values2.Add(t);
             }
-            Label.Value = new ObservableCollection<string>();
-            LineSeries temp = new LineSeries();
-            temp.Values = (IChartValues)values;
-            this.PreviousMonth.Value = temp;
-            LineSeries temp2 = new LineSeries();
-            temp.Values = (IChartValues)values2;
-            this.ThisMonth.Value = temp2;
+            for (int i = 1; i < 101; i++) { 
+                Label.Add(i+"일");
+            }
+            var series1 = new LineSeries
+            {
+                Title = "지난달",
+                Values = values
+            };
+
+            var series2 = new LineSeries
+            {
+                Title = "이번달",
+                Values = values2
+            };
+            SeriesCollection.Add(series2);
+            SeriesCollection.Add(series1);
         }
-        
+
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
             return true;
